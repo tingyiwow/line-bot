e088b0f686cf4c6e52d735f2285d1df234a4da72
tingyiwow <r07444005@ntu.edu.tw>
2021-05-30 18:30:34 +0800

    update reply2

--- app.py
+++ app.py
@@ -40,16 +40,28 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = '抱歉沒猜到hehe'
+    s = '抱歉我看不懂hehe'
 
-    if msg == '和牛涮':
+    if msg == '你好':
+        s = '小然好！今天想吃什麼？選項有：和牛涮、屋馬、河童'
+    elif msg == '和牛涮':
         s = '卡架！'
     elif msg == '屋馬':
         s = '卡架！'
     elif msg == '河童':
         s = '想得美:P'
     elif msg == '吃飽了':
-        s = '祝小然26歲生日快樂！'
+        s = '那你今天想要一起做什麼事？選項有：夜衝看夜景、看電影、在家裡吃鹹酥雞打電動'
+    elif msg == '夜衝看夜景':
+        s = '好ㄚ上車吧！'
+    elif msg == '看電影':
+        s = '那好吧:( 你選'
+    elif msg == '在家裡吃鹹酥雞打電動':
+        s = '好！那還要配瓜瓜奶！'
+    elif msg == '開心':
+        s = '那你今天幸福嗎？'
+    elif msg == '幸福':
+        s = '庭亦也幸福～謝謝小然的陪伴！比起生日快樂，更希望你身體健康、天天開心哦love you 3000'
 
     line_bot_api.reply_message(
         event.reply_token,
