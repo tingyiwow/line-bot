0da644e15ade036d644c7a8569137bf44c6a7fe6
tingyiwow <r07444005@ntu.edu.tw>
2021-05-30 17:53:43 +0800

    add ''

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = 哈囉嘻嘻
+    s = '哈囉嘻嘻'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s)) # 回復使用者傳來的訊息
