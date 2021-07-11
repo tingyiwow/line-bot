d08564707d444e8ed4fb8e1f8a5f2407658abbe3
tingyiwow <r07444005@ntu.edu.tw>
2021-07-11 18:19:59 +0800

    update reply 3

--- app.py
+++ app.py
@@ -43,30 +43,38 @@ def handle_message(event):
     s = '抱歉我看不懂hehe'
 
     if msg == '你好':
-        s = '小然好！今天想吃什麼？選項有：和牛涮、屋馬、河童'
+        s = 'Hi! Kerorojj! May I have a day with you?  yes/no'
+    elif msg == 'yes':
+        s = 'Nothing more that I can choose <3 那你想吃什麼咧？選項有：和牛涮、屋馬、必勝客芝心披薩、河童'
     elif msg == '和牛涮':
-        s = '卡架！'
+        s = '卡架！（吃飽後記得跟我說”吃飽了“呦）'
     elif msg == '屋馬':
-        s = '卡架！'
+        s = '卡架！（吃飽後記得跟我說”吃飽了“呦）'
+    elif msg == '必勝客芝心披薩':
+        s = '卡架！（吃飽後記得跟我說”吃飽了“呦）'
     elif msg == '河童':
-        s = '想得美:P'
+        s = '：）想被揍嗎？'
     elif msg == '吃飽了':
-        s = '那你今天想要一起做什麼事？選項有：夜衝看夜景、看電影、在家裡吃鹹酥雞打電動'
-    elif msg == '夜衝看夜景':
-        s = '好ㄚ上車吧！'
+        s = '那你今天還想要一起做什麼事？選項有：出去99夜衝看夜景、看電影、在家裡吃鹹酥雞打電動'
+    elif msg == '出去99夜衝看夜景':
+        s = '好ㄚ上車吧！ 你今天開心嗎？'
     elif msg == '看電影':
-        s = '那好吧:( 你選'
+        s = '那好吧～ 你選！ 你今天開心嗎？'
     elif msg == '在家裡吃鹹酥雞打電動':
-        s = '好！那還要配瓜瓜奶！'
+        s = '好！那還要配瓜瓜奶！ 你今天開心嗎？'
     elif msg == '開心':
         s = '那你今天幸福嗎？'
     elif msg == '幸福':
-        s = '庭亦也幸福～謝謝小然的陪伴！比起生日快樂，更希望你身體健康、天天開心哦love you 3000'
+        s = '庭亦也幸福～謝謝然ㄍ的陪伴！26生日快樂（好老）love you 3000'
+    else:
+        s = ':)想被揍嗎？'
+
 
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=s)) # 回復使用者傳來的訊息
 
+# 想要加什麼就到https://github.com/line/line-bot-sdk-python來找
 
 if __name__ == "__main__": # 不希望import的時候就執行程式碼，確定檔案是被執行而不是被載入
     app.run()
